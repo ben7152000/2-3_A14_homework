def46c65ecfb33fb961803578fe76a979c25463a
Ben Chou <ben7152000@gmail.com>
2021-02-15 14:50:50 +0800

    create html and css

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
-const PORT = 3000
+// Mongoose
+require('./config/mongoose')
+
+// Normal Setting
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+// Express-Handlebars
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: { same: (a, b) => a === b } }))
+app.use(express.static('public'))
+
+// Body-Parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('ok')
+  res.render('index')
 })
 
+// Listen
 app.listen(PORT, console.log(`The server is running on localhost:${PORT}`))
